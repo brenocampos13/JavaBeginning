03b86b377658bae6903994703a9b696e1853d9e3
brenocampos13 <canaldarkbreno@gmail.com>
2025-10-02 19:23:00 -0300

    nova atualização

---
+++ src/VotoObrigatorio.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class VotoObrigatorio {
+    static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Digite sua idade:");
+        int idade = keyboard.nextInt();
+        if (idade >= 16 && idade <= 17) {
+            System.out.println("O voto é opcional!");
+        } else if (idade >= 18 && idade <= 70){
+            System.out.println("O voto é obrigatório!");
+        } else if (idade > 70 && idade <= 100){
+            System.out.println("O voto é opcional!");
+        } else if (idade >= 100){
+            System.out.println("Você realmente tem essa idade? KEKW");
+        } else {
+            System.out.println("Você ainda não vota!");
+        }
+    }
+}
