be6d0911b49b58fa069e7de386ee313169576cc6
brenocampos13 <canaldarkbreno@gmail.com>
2025-10-02 12:39:55 -0300

    nova atualização

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
 
-public class Calculadora {
+public class Calculadora{
     static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Digite um número: ");
-        int numero = keyboard.nextInt();
-        int parouimpar = numero % 2;
-        double yoCube1 = numero;
-        double exp1 = 3;
-        if (parouimpar == 0) {
+        System.out.println("Type a number: ");
+        String numero1 = keyboard.nextLine();
+        int numero = Integer.parseInt(numero1);
+        float parouimpar = numero % 2;
+        double expoente = 3;
+        if (parouimpar == 0){
             System.out.println("O número é par!");
         } else {
             System.out.println("O número é impar!");
         }
         double raiz = Math.sqrt(numero);
-        double cube2 = Math.pow(yoCube1, exp1);
+        double cube2 = Math.pow(numero, expoente);
         double cubic = Math.cbrt(numero);
-        double absolute = Math.abs(numero);
-        System.out.println("A raiz quadrada de " + numero + " é: " + raiz);
-        System.out.printf("O número " + numero + " elevado ao cubo é: %.2f\n", cube2);
-        System.out.printf("A raiz cúbica de " + numero + " é: %.2f\n", cubic);
-        System.out.printf("O valor absoluto de: " + numero + " é %.0f", absolute);
+        double absolut = Math.abs(numero);
+        System.out.printf("A raiz de %d é igual á %.2f\n", numero, raiz);
+        System.out.printf("O %d elevado á %.0f é igual á %.2f!\n", numero, expoente, cube2);
+        System.out.printf("A raiz cúbica de %d é igual á %.2f\n", numero, cubic);
+        System.out.printf("O número absoluto de %d é igual á %.0f\n", numero, absolut);
     }
 }
