f4bc1dba9ff0ca49f5293a6b567a8e0b8184eafd
Breno Campos Franco <canaldarkbreno@gmail.com>
2025-10-03 18:00:27 -0300

    nova atualização

---
+++ src/Fatoracao.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Fatoracao {
+    static void main(String[] args) {
+        Scanner t = new Scanner(System.in);
+        int numero = t.nextInt();
+        int f = 1;
+        int c = numero;
+        while (c >= 1) {
+            f *= c;
+            c--;
+        }
+        System.out.println(f);
+    }
+}
