3843ce3b286f0bb0a599f9193d8fda42855a9695
Breno Campos Franco <canaldarkbreno@gmail.com>
2025-10-04 18:25:36 -0300

    nova atualização

--- src/Tabuada2.java
+++ src/Tabuada2.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 
 public class Tabuada2 {
     static void main(String[] args) {
-        Scanner t = new Scanner(System.in);
-        int contador = 1;
+        Scanner t = new Scanner (System.in);
         System.out.println("Digite um número:");
         int numero = t.nextInt();
-
+        int contador = 1;
         while (contador <= 10){
             int n1 = numero * contador;
             System.out.printf("%d x %d = %d\n", numero, contador, n1);
