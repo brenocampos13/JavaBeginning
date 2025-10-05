3a0fd7a096a16712a59425292f8ee09fb005673f
Breno Campos Franco <canaldarkbreno@gmail.com>
2025-10-05 18:18:19 -0300

    nova atualização

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -6,20 +6,25 @@ public class Calculadora{
         System.out.println("Type a number: ");
         String numero1 = keyboard.nextLine();
         int numero = Integer.parseInt(numero1);
-        float parouimpar = numero % 2;
-        double expoente = 3;
-        if (parouimpar == 0){
-            System.out.println("O número é par!");
-        } else {
-            System.out.println("O número é impar!");
-        }
         double raiz = Math.sqrt(numero);
-        double cube2 = Math.pow(numero, expoente);
         double cubic = Math.cbrt(numero);
         double absolut = Math.abs(numero);
+        Parouimpar(numero);
+        Expoente(numero);
         System.out.printf("A raiz de %d é igual á %.2f\n", numero, raiz);
-        System.out.printf("O %d elevado á %.0f é igual á %.2f!\n", numero, expoente, cube2);
         System.out.printf("A raiz cúbica de %d é igual á %.2f\n", numero, cubic);
         System.out.printf("O número absoluto de %d é igual á %.0f\n", numero, absolut);
+    }
+    public static void Parouimpar(int numero){
+        if (numero % 2 == 0){
+            System.out.printf("O número %d é par!\n", numero);
+        } else {
+            System.out.printf("O número %d é par!\n", numero);
+        }
+    }
+    public static void Expoente(double numero){
+        double expoente = 3;
+        double cube2 = Math.pow(numero, expoente);
+        System.out.printf("O %.0f elevado á %.0f é igual á %.3f\n", numero, expoente, cube2);
     }
 }
