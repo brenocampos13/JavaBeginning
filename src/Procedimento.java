290d9634a2588969b1e47dc15ecc0fd2651b2405
Breno Campos Franco <canaldarkbreno@gmail.com>
2025-10-05 15:51:01 -0300

    nova atualização

--- src/Procedimento.java
+++ src/Procedimento.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Procedimento {
-    static int x1;
+    static int x1; // x1 é uma variável estática que nunca muda
     static void main(String[] args) {
         Scanner t = new Scanner(System.in);
         System.out.println("Digite um número:");
@@ -11,6 +11,6 @@ public class Procedimento {
         System.out.println(x1);
     }
     public static void ParOuImpar ( int A, int B) {
-        x1 = A + B;
+        x1 = A + B; // Feita a soma a variável estática recebe e aloca essa soma
     }
 }
