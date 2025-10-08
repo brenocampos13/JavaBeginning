58d81b4c69e7466bfab82a6b975e1672df145ada
Breno Campos Franco <canaldarkbreno@gmail.com>
2025-10-08 01:45:48 -0300

    nova atualização

--- src/com/personal/beginning/TratamentoString.java
+++ src/com/personal/beginning/TratamentoString.java
@@ -1,4 +1,13 @@
 package com.personal.beginning;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+
 public class TratamentoString {
+    static void main(String[] args) {
+        String nome = "Breno";
+        System.out.println(nome.length());
+
+    }
 }
