0e85a11b54dcd972e07a13604f72cbc226653869
Breno Campos Franco <canaldarkbreno@gmail.com>
2025-10-10 16:13:05 -0300

    nova atualização

--- src/Passos.java
+++ src/Passos.java
@@ -4,11 +4,11 @@ public class Passos {
     static void main(String[] args) {
         int numero1, numero2, numero3;
         Scanner t = new Scanner(System.in);
-        System.out.println("Digite o primeiro numero:");
+        System.out.println("Digite onde começará o loop:");
         numero1 = t.nextInt();
-        System.out.println("Digite o segundo número:");
+        System.out.println("Digite onde terminará o loop:");
         numero2 = t.nextInt();
-        System.out.println("Digite os passos:");
+        System.out.println("Digite os passos do loop:");
         numero3 = t.nextInt();
         for (int i = numero1; i <= numero2; i += numero3){
             System.out.println(i);
