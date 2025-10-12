a69cc3317ec22928e59e813fbf7ddd69fbce42e5
Breno Campos Franco <canaldarkbreno@gmail.com>
2025-10-12 00:51:22 -0300

    nova atualização

--- src/Vetores.java
+++ src/Vetores.java
@@ -1,9 +1,8 @@
 public class Vetores {
     static void main(String[] args) {
         int[] n = {1, 3, 5, 7, 9, 11};
-        System.out.println(n[0]);
-        for(int c = n[0]; c <= n[5]; c++){
-            System.out.println(n[c]);
+        for (int i = n[0]; i <= n[5]; i++){
+            System.out.println(n[i]);
         }
     }
 }
