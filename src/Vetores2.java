4f9aabb95c89a43d0829dce91155d67d832d9a15
Breno Campos Franco <canaldarkbreno@gmail.com>
2025-10-12 11:48:27 -0300

    nova atualização

---
+++ src/Vetores2.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Vetores2 {
+    static void main(String[] args) {
+        int[] n = {1, 4, 3, 7, 9, 8};
+        Arrays.sort(n);
+        for (int valor: n){
+            System.out.println(valor);
+        }
+    }
+}
