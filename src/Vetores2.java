a4c549acabd6a95a147051467929c065e1a1c69f
Breno Campos Franco <canaldarkbreno@gmail.com>
2025-10-12 14:18:38 -0300

    nova atualização

--- src/Vetores2.java
+++ src/Vetores2.java
@@ -1,11 +1,18 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Vetores2 {
     static void main(String[] args) {
-        int[] n = {1, 4, 3, 7, 9, 8};
-        Arrays.sort(n);
-        for (int valor: n){
-            System.out.println(valor);
+        Scanner t = new Scanner(System.in);
+        System.out.println("Digite o tamanho do array:");
+        int tam = t.nextInt();
+        int[] n = new int [tam];
+        System.out.println("Digite o valor inicial do array:");
+        int valor = t.nextInt();
+        Arrays.fill(n, valor);
+        System.out.printf("Criamos a array com %d casas e todas com o número %d como inicial!\n", tam, valor);
+        for (int i: n) {
+            System.out.print(valor + " ");
         }
     }
 }
