edc161b581ba8ebc90f6afbc515cdba542655d08
Breno Campos Franco <canaldarkbreno@gmail.com>
2025-10-13 23:22:36 -0300

    nova atualização

---
+++ src/Vetores.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Vetores {
+    static int i;
+    static void main(String[] args) {
+        Scanner t = new Scanner(System.in);
+        System.out.println("Em que ano estamos?");
+        String ano = t.nextLine();
+        String[] bissexto = {"2020", "2024", "2028", "2032"};
+        String[] mes = {"Jan", "Fev", "Mar", "Abr", "Mai", "Jun",
+                "Jul", "Ago", "Set", "Nov", "Dez"};
+        if (Arrays.asList(bissexto).contains(ano)){
+            System.out.println("O ano é bissexto!");
+            int[] dias = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+            for (i = 0; i <= mes.length - 1; i++){
+                System.out.printf("O mês %s tem %d dias!\n", mes[i], dias[i]);
+            }
+        } else {
+            System.out.println("O ano não é bissexto!");
+            int[] dias = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+            for (i = 0; i <= mes.length - 1; i++) {
+                System.out.printf("O mês %s tem %d dias!\n", mes[i], dias[i]);
+            }
+        }
+    }
+}
