58ea229e67067828d718edc0b2e0302f6ecef7af
Breno Campos Franco <canaldarkbreno@gmail.com>
2025-10-13 23:06:11 -0300

    nova atualização

---
+++ src/AdicionarElementos.java
@@ -0,0 +1,5 @@
+public class AdicionarElementos {
+    static void main(String[] args) {
+
+    }
+}
--- src/Vetores.java
+++ src/Vetores.java
@@ -5,22 +5,17 @@ public class Vetores {
         Scanner t = new Scanner(System.in);
         System.out.println("Em que ano estamos?");
         String ano = t.nextLine();
-        String[] bissexto = {"2020", "2024", "2028", "2032", "2036", "2040"};
-        String[] mes = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
-                "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
-
-        if (Arrays.asList(bissexto).contains(ano)) {
-            System.out.println("O ano é bissexto!");
-            int[] totB = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-            for (int c = 0; c <= mes.length - 1; c++) {
-                System.out.printf("O mês de %s tem %d dias!\n",mes[c], totB[c]);
+        String[] bissexto = {"2020", "2024", "2028", "2032"};
+        String[] mes = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+        if (Arrays.asList(bissexto).contains(ano)){
+            System.out.println("O ano é bissexto");
+            int[] dias = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+            for (int i = 0; i <= mes.length - 1; i++){
+                System.out.printf("O mês %s tem %d dias!\n", mes[i], dias[i]);
             }
         } else {
             System.out.println("O ano não é bissexto!");
-            int[] tot = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-            for (int c = 0; c <= mes.length - 1; c++) {
-                System.out.printf("O mês de %s tem %d dias!\n",mes[c], tot[c]);
-            }
+
         }
     }
 }
