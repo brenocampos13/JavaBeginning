f0a1fab5f055c378b42a6aae58bc835556c644a9
Breno Campos Franco <canaldarkbreno@gmail.com>
2025-10-17 01:01:12 -0300

    nova atualização

--- src/CadastroAlunos.java
+++ src/CadastroAlunos.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class CadastroAlunos {
@@ -8,6 +7,8 @@ public class CadastroAlunos {
         float[] nota1 = new float[3];
         float[] nota2 = new float[3];
         double[] media = new double[3];
+        int qntdAlunos = 0;
+        float mediaTotal = 7;
         for (int i = 0; i <= nomes.length - 1; i++){
             System.out.printf("Digite um nome (%d/3):\n", i + 1);
             nomes[i] = t.nextLine();
@@ -15,13 +16,17 @@ public class CadastroAlunos {
             nota1[i] = t.nextFloat();
             System.out.printf("Digite a segunda nota de %s:\n", nomes[i]);
             nota2[i] = t.nextFloat();
-            media[i] = nota1[i] + nota2[i] / 2;
+            media[i] = (nota1[i] + nota2[i]) / 2;
             t.nextLine();
+            if (media[i] >= mediaTotal){
+                qntdAlunos += 1;
+            }
         }
         System.out.println("ALUNOS CADASTRADOS:");
         System.out.println("===================");
         for (int c = 0; c <= nomes.length - 1; c++){
             System.out.printf("A média do aluno %s é %.2f\n", nomes[c], media[c]);
         }
+        System.out.printf("Foram encontrados %d alunos acima da média que é: %.2f\n", qntdAlunos, mediaTotal);
     }
 }
