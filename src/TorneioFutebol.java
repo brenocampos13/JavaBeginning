dc18b364c69a1770f9ceb39b4942dae84c98134f
Breno Campos Franco <canaldarkbreno@gmail.com>
2025-10-20 19:29:58 -0300

    nova atualização

--- src/TorneioFutebol.java
+++ src/TorneioFutebol.java
@@ -7,30 +7,31 @@ public class TorneioFutebol {
         String[] times = new String[3];
         int qntdTimes = 0;
 
-        System.out.println("Digite um time ('fim' encerra):");
+        System.out.println("Digite 3 times:");
 
-        while (true) {
-            System.out.println("Digite o time (Max. 3):");
+        while (true){
+            System.out.println("Digite um time, 'fim' encerra!:");
             String nome = t.nextLine().trim();
 
-            if (nome.equalsIgnoreCase("fim")) {
+            if (nome.equalsIgnoreCase("fim")){
                 break;
             }
 
-            if (nome.isEmpty()) {
-                System.out.println("Termo incorreto, digite novamente!");
-                continue;
+            if (nome.isEmpty()){
+                System.out.println("Termo inválido, digite novamente!");
             }
-            boolean repetido = false;
-            for (int i = 0; i < qntdTimes; i++) {
 
-                if (times[i].equalsIgnoreCase(nome)) {
+            boolean repetido = false;
+            for (int i = 0; i < qntdTimes; i++){
+
+                if (times[i].equalsIgnoreCase(nome)){
                     repetido = true;
                     break;
                 }
             }
-            if (repetido) {
-                System.out.println("Este time já está na lista!");
+
+            if (repetido){
+                System.out.println("Este nome já está na lista!");
             } else {
                 times[qntdTimes] = nome;
                 qntdTimes++;
@@ -39,9 +40,9 @@ public class TorneioFutebol {
 
         System.out.println("CONFRONTOS:");
 
-        for (int i = 0; i < qntdTimes; i++) {
-            for (int j = i + 1; j < qntdTimes; j++) {
-                System.out.println(times[i] + "[] X []" + times[j]);
+        for (int i = 0; i < qntdTimes; i++){
+            for (int j = i + 1; j < qntdTimes; j++){
+                System.out.println(times[i] + "[] x []" + times[j]);
             }
         }
     }
