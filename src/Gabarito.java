798a18b114707200cefe950d1a1688daeb203aa8
Breno Campos Franco <canaldarkbreno@gmail.com>
2025-10-23 18:28:25 -0300

    nova atualização

--- src/Gabarito.java
+++ src/Gabarito.java
@@ -29,14 +29,15 @@ public class Gabarito {
 
             qntdAlunos++;
         }
+
         float nota = 0;
         int qntdAcertos = 0;
         float notaSala = 0;
 
-        for (int i = 0; i < qntdAlunos; i++) {
+        for (int i = 0; i < respAlunos.length; i++) {
             System.out.printf("Digite o gabarito da prova de %s\n", nomeAlunos[i]);
 
-            for (int j = 0; j < qntdResp; j++) {
+            for (int j = 0; j < respAlunos.length; j++) {
                 respAlunos[i][j] = t.nextLine().trim();
                 if (respAlunos[i][j].equals(gabarito[i])) {
                     nota += 2;
