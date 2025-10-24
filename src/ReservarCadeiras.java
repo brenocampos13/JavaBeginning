666ead429e87a784d835bc0ed56021949da77d9a
Breno Campos Franco <canaldarkbreno@gmail.com>
2025-10-24 19:43:07 -0300

    FICOU PRONTO!

---
+++ src/ReservarCadeiras.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ReservarCadeiras {
+    static void main(String[] args) {
+        Scanner t = new Scanner(System.in);
+
+        String[] cadeiras = {"B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10"};
+        
+    }
+}
