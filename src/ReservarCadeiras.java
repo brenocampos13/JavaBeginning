6aacee0a0ff59d9b22b192f736e7c971e9837a1f
Breno Campos Franco <canaldarkbreno@gmail.com>
2025-10-25 19:06:34 -0300

    nova atualização

--- src/ReservarCadeiras.java
+++ src/ReservarCadeiras.java
@@ -1,42 +1,37 @@
 import java.util.Scanner;
 import java.util.Arrays;
 import java.util.ArrayList;
-// Ja temos a string de cadeiras
-// The system ask the chair desired until the negative answer
-//
 public class ReservarCadeiras {
     static void main(String[] args) {
         Scanner t = new Scanner(System.in);
 
-        String[] cadeiras = {"B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10"};
-        String[] cadeiras2 = {"B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10"};
+        ArrayList cadeiras = new ArrayList<>(10);
+
         System.out.println("CADEIRAS DISPONÍVEIS:");
-        for (int i = 0; i < cadeiras.length; i++) {
-            System.out.printf("[%s] ", cadeiras[i]);
+
+        for (int i = 0; i <= 9; i++) {
+            cadeiras.add("B" + (i + 1));
         }
+        System.out.print(cadeiras);
         System.out.println("\n===================================================");
 
-        for (int i = 0; i < cadeiras.length; i++){
-            System.out.println("Deseja reservar uma cadeira? [S/N]");
+        for (int i = 0; i < cadeiras.size(); i++) {
+            System.out.println("\nDeseja reservar uma cadeira? [S/N]");
             String resp = t.nextLine().trim();
 
-            if (resp.equalsIgnoreCase("N")){
+            if (resp.equalsIgnoreCase("N")) {
                 break;
             }
 
             System.out.println("Qual a cadeira a ser reservada?");
             String reserva = t.nextLine().trim();
 
-            if (cadeiras[i].equalsIgnoreCase("==")){
-                System.out.printf("A cadeira %s já está reservada!", cadeiras2[i]);
-                System.out.println("\n");
+            if (Arrays.asList(cadeiras).contains(reserva)) {
+                cadeiras.remove(reserva);
             }
 
-            if (Arrays.asList(cadeiras).contains(reserva)){
-                cadeiras[i] = "==";
-            }
-
-            System.out.printf("[%s] ", cadeiras[i]);
+            System.out.println(cadeiras);
         }
     }
 }
+//"B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10"
