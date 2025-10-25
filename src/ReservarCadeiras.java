ceadf3fb2b65173d0a84201846fefcb4ed966aaf
Breno Campos Franco <canaldarkbreno@gmail.com>
2025-10-25 12:11:38 -0300

    nova atualização

--- src/ReservarCadeiras.java
+++ src/ReservarCadeiras.java
@@ -1,10 +1,33 @@
 import java.util.Scanner;
-
+import java.util.Arrays;
+import java.util.ArrayList;
+// Ja temos a string de cadeiras
+// The system ask the chair desired until the negative answer
+//
 public class ReservarCadeiras {
     static void main(String[] args) {
         Scanner t = new Scanner(System.in);
 
         String[] cadeiras = {"B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10"};
-        
+
+        System.out.println("CADEIRAS DISPONÍVEIS:");
+        for (int i = 0; i < cadeiras.length; i++) {
+            System.out.printf("[%s] ", cadeiras[i]);
+        }
+        System.out.println("\n===================================================");
+
+        while (true){
+            System.out.println("Deseja reservar uma cadeira? [S/N]");
+            String resp = t.nextLine().trim();
+
+            if (resp.equalsIgnoreCase("N")){
+                break;
+            }
+            System.out.println("Qual a cadeira a ser reservada?");
+            String reserva = t.nextLine().trim();
+            if(Arrays.asList(cadeiras).contains(reserva)){
+
+            }
+        }
     }
 }
