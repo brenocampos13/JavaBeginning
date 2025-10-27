3ef1daf338d2985c68851ac745efcc2cd6f8725d
Breno Campos Franco <canaldarkbreno@gmail.com>
2025-10-27 16:15:18 -0300

    nova atualização

--- src/MatrizesArmazenar.java
+++ src/MatrizesArmazenar.java
@@ -4,28 +4,22 @@ public class MatrizesArmazenar {
     static void main(String[] args) {
         Scanner t = new Scanner(System.in);
 
-        int[][] numerosPares = new int[3][3];
         ArrayList<Integer> pares = new ArrayList<>();
         ArrayList<Integer> impares = new ArrayList<>();
+        int[] numerosPares = new int[9];
 
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 2; j++){
-                numerosPares[i][j] = t.nextInt();
-                int numerosPares1 = numerosPares[i][j];
+        for (int i = 0; i < 9; i++){
+            numerosPares[i] = t.nextInt();
 
-                if (numerosPares1 % 2 == 0){
-                    pares.add(numerosPares1);
-                } else {
-                    impares.add(numerosPares1);
-                }
+            if (numerosPares[i] % 2 == 0){
+                pares.add(numerosPares[i]);
+
+            } else {
+                impares.add(numerosPares[i]);
+
             }
         }
-        for (int j = 0; j < 3; j++){
-            for (int c = 0; c < 3; c++){
-                System.out.print(numerosPares[j][c] + " ");
-            }
-            System.out.print(pares);
-            System.out.println(impares);
-        }
+        System.out.print(pares);
+        System.out.println(impares);
     }
 }
